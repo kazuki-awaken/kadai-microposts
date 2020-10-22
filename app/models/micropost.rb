e14eb8bb70c785000dffb4c4547d5d324fcc99ac
gie <macchaner@gmail.com>
2020-10-22 07:57:57 +0000

    お気に入り機能の実装

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
-  
   validates :content, presence: true, length: { maximum: 255 }
+  
+  belongs_to :user
+  has_many :reverses_of_favorite, class_name: 'Favorite'
+  has_many :favored, through: :reverses_of_favorite, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
+    has_many :favorites
+    has_many :favoring, through: :favorites, source: :micropost
+    
     def follow(other_user)
       unless self == other_user
         self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,5 +33,20 @@ class User < ApplicationRecord
     def feed_microposts
       Micropost.where(user_id: self.following_ids + [self.id])
     end
+    
+    
+    def favor(micropost)
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+    
+    def dissolve(micropost)
+     favorite = self.favorites.find_by(micropost_id: micropost.id)
+     favorite.destroy if favorite
+    end
+  
+    def favoring?(micropost)
+     self.favoring.include?(micropost)
+    end
+  
 
 end
